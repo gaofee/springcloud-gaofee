8307e48bb2b97225fc405ff2c1c4c45db5c32632
feege <79527743@qq.com>
2022-06-01 16:24:16 +0800

    es,高亮分页多条件复合查询

--- search-service/src/test/java/com/gaofei/es/SearchTest.java
+++ search-service/src/test/java/com/gaofei/es/SearchTest.java
@@ -1,14 +1,17 @@
 package com.gaofei.es;
 
+import cn.hutool.json.JSONUtil;
 import com.gaofei.es.entity.User;
 import com.gaofei.es.repositories.UserRep;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.junit.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -49,10 +52,28 @@ public class SearchTest {
     public void search(){
 
         /**
-         * {
+         *{
          *   "query": {
-         *     "match": {
-         *       "name": "中国"
+         *     "bool": {
+         *       "must":[
+         *                {
+         *           "term": {
+         *                   "name": "中国"
+         *                }
+         *         },
+         *        {
+         *           "term": {
+         *                   "id": "2"
+         *                }
+         *         }
+         * 	  ],
+         * 	  "must_not":[
+         *        {
+         *           "term": {
+         *                   "name": "高飞"
+         *                }
+         *         }
+         * 	  ]
          *     }
          *   },
          *   "highlight": {
@@ -66,6 +87,22 @@ public class SearchTest {
          *   }
          * }
          */
+        String name = "中国";
+        Integer id = 2;
+        Integer pageNum = 3;
+        Integer pageSize = 6;
+
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        //注意,拼这些条件的时候,一定要加上条件的非空判卷
+        if(null!=name&&name!=""){
+            boolQuery.must(QueryBuilders.termQuery("name","中国"));
+            boolQuery.mustNot(QueryBuilders.termQuery("name","高飞"));
+        }
+        //注意,拼这些条件的时候,一定要加上条件的非空判卷
+        if(id!=null&&id !=0){
+//            boolQuery.must(QueryBuilders.termQuery("id",2));
+        }
+
         //构建高亮的feild字段
         HighlightBuilder.Field fields = new HighlightBuilder
                 .Field("*")
@@ -74,11 +111,11 @@ public class SearchTest {
 
         NativeSearchQuery nativeSearchQuery =new  NativeSearchQueryBuilder()
                 .withIndices("users").withTypes("user") //指定索引库的名称,指定类型
-                .withQuery(QueryBuilders.termQuery("name","中国"))
-                .withPageable(PageRequest.of(0,3)) //分页查询
+                .withQuery(boolQuery) //拼装条件
+                .withPageable(PageRequest.of(pageNum-1,pageSize)) //分页查询
                 .withHighlightFields(fields)
                 .build();
-
+        //这个对象就是分页的对象,类似于pagehelper中的pageInfo对象
         AggregatedPage<User> aggregatedPage =  elasticsearchTemplate.queryForPage(nativeSearchQuery, User.class,new SearchResultMapper(){
 
             @Override
@@ -96,7 +133,7 @@ public class SearchTest {
                     Map<String, HighlightField> highlightFields = hit.getHighlightFields();
                     Set<Map.Entry<String, Object>> entries = sourceAsMap.entrySet();
                     for (Map.Entry<String, Object> entry : entries) {
-                        //通过key获取高亮字段
+                        //通过key获取高亮字段                                   id  name
                         HighlightField highlightField = highlightFields.get(entry.getKey());
                         //将找到的高亮字段的内容  替换到原生结果集的map中
                         if (highlightField != null) {
@@ -114,8 +151,11 @@ public class SearchTest {
                     tPoem.setUsername((String) sourceAsMap.get("username"));
                     poemList.add(tPoem);
                 }
+
+                long totalHits = searchResponse.getHits().getTotalHits();
+
                 //将封装好的结果集 传给 一个数据传输的载体对象
-                return new AggregatedPageImpl(poemList);
+                return new AggregatedPageImpl(poemList,pageable,totalHits);
             }
 
             @Override
@@ -125,7 +165,12 @@ public class SearchTest {
         });
 
         System.out.println(aggregatedPage.getContent());
+        System.out.println("总页数:"+aggregatedPage.getTotalPages());
+        System.out.println("总记录数:"+aggregatedPage.getTotalElements()+" 条");
+        System.out.println("当前页:"+(aggregatedPage.getNumber()+1));
+        System.out.println("每页显示:"+aggregatedPage.getSize());
 
+        System.out.println(JSONUtil.toJsonStr(aggregatedPage));
         //===================================================
         /**
          * {
